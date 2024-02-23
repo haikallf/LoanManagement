c0b68dfd4ec4e668aa49585303154d489d3985ee
haikallf <haikallzrd@gmail.com>
2024-02-23 20:24:49 +0700

    init

---
+++ LoanManagement/LoanManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanManagementApp.swift
+//  LoanManagement
+//
+//  Created by Haikal Lazuardi Fadil on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LoanManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
